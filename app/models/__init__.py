535f4554112232500ac9e6210ee033a921196e87
Lee-YouJ <dia2yj03@gmail.com>
2026-05-23 21:46:11 +0900

    fix: SQLAlchemy 관계 해석 오류 및 posts 테이블 마이그레이션 누락 수정
    - models/__init__.py에 누락된 모델 클래스 일괄 등록
      (BoardRegistry, ChatRoom, FriendRequest, GroupMember, Post 등)
    - SQLAlchemy relationship 초기화 시 클래스를 찾지 못하던 500 오류 해결
    - posts 테이블에 image_urls 컬럼 직접 추가 (마이그레이션 누락분)

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,12 @@
 # models 폴더 안에 실제로 존재하는 파일명에 맞춰서 추가해 주세요.
 
 from .calendar import UserEvent as CalendarEvent
+from .board_registry import BoardRegistry
+from .room import ChatRoom, RoomMember
+from .friend_request import FriendRequest
+from .group_member import GroupMember
+from .post import Post, PostLike, PostComment
+from .user import User
+from .group import Group
+from .message import Message
+from .invite import InviteCode
